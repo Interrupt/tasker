7b59da5ce98df9de1e244bbb60ff8f69be999d68
Chad Cuddigan <cuddigan@gmail.com>
2014-11-18 23:27:27 -0800

    Added method to index objects to the search client

--- tasks-client/src/main/java/com/interrupt/elasticsearch/SearchClient.java
+++ tasks-client/src/main/java/com/interrupt/elasticsearch/SearchClient.java
@@ -4,6 +4,7 @@ import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.JestResult;
 import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.Index;
 import io.searchbox.core.Search;
 
 import java.util.logging.Logger;
@@ -25,26 +26,35 @@ public class SearchClient {
     }
 
     public JestResult Search(String query) {
-        String fullQuery =
-                "{\n" +
-                "  \"query\": {\n" +
-                "  \"multi_match\" : {\n" +
-                "    \"query\": \"" + query + "\", \n" +
-                "    \"fields\": [ \"name^3\", \"owner\" ] \n" +
-                "  }\n" +
-                " }\n" +
-                "}";
-
-        Search search = new Search.Builder(fullQuery)
-                .addIndex("tasks")
-                .addType("task")
-                .build();
-
         try {
+            String fullQuery =
+                    "{\n" +
+                    "  \"query\": {\n" +
+                    "  \"multi_match\" : {\n" +
+                    "    \"query\": \"" + query + "\", \n" +
+                    "    \"fields\": [ \"title^3\", \"body\" ] \n" +
+                    "  }\n" +
+                    " }\n" +
+                    "}";
+
+            Search search = new Search.Builder(fullQuery)
+                    .addIndex("tasks")
+                    .addType("task")
+                    .build();
+
             return client.execute(search);
         } catch (Exception e) {
             logger.severe(e.getMessage());
             return null;
+        }
+    }
+
+    public void Index(String key, Object object) {
+        try {
+            Index index = new Index.Builder(object).index("tasks").type("task").id(key).build();
+            client.execute(index);
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
         }
     }
 }
--- tasks-client/src/test/java/com/interrupt/elasticsearch/SearchIT.java
+++ tasks-client/src/test/java/com/interrupt/elasticsearch/SearchIT.java
@@ -1,15 +1,23 @@
 package com.interrupt.elasticsearch;
 
+import com.interrupt.tasks.model.Task;
 import io.searchbox.client.JestResult;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
 public class SearchIT {
+
     @Test
-    public void shouldSetupClient() {
+    public void shouldIndexTask() {
         SearchClient client = new SearchClient();
-        JestResult hits = client.Search("Test");
+        client.Index("testkey", new Task("Test Search Title", "Test Search Body"));
+    }
+
+    @Test
+    public void shouldExecuteSearch() {
+        SearchClient client = new SearchClient();
+        JestResult hits = client.Search("Search");
 
         assertNotNull(hits);
     }
